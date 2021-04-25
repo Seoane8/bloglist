9f47873f168e1254b74c5b095173e0f62ed4dffd
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-04-25 19:51:00 +0200

    feat: delete a blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,16 +1,25 @@
 const blogsRouter = require('express').Router()
 const { Blog } = require('../models')
 
-blogsRouter.get('/', async (request, response, next) => {
+blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const newBlog = new Blog(request.body)
 
   const savedBlog = await newBlog.save()
   response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params
+
+  const deletedBlog = await Blog.findByIdAndDelete(id)
+
+  if (!deletedBlog) return response.status(404).end()
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
