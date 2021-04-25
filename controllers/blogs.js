a7c115f369a7222cffeab5e64c461fa221016eb9
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-04-25 21:16:11 +0200

    feat: update a blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -22,4 +22,18 @@ blogsRouter.delete('/:id', async (request, response) => {
   response.status(204).end()
 })
 
+blogsRouter.put('/:id', async (request, response) => {
+  const { id } = request.params
+  const blog = request.body
+
+  const updatedBlog = await Blog
+    .findByIdAndUpdate(
+      id,
+      blog,
+      { new: true, runValidators: true })
+
+  if (!updatedBlog) return response.status(404).end()
+  response.json(updatedBlog)
+})
+
 module.exports = blogsRouter
