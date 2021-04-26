cdb8b04886634b3d6979dfd6e03b88c33677a74b
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-04-26 21:54:20 +0200

    build: convert in monorepo multipackage with frontend and backend

---
+++ app/build/precache-manifest.e14ad4f1af86b264c3eaddeb27ae3492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d44102be70dd661d44d55c35d0f52b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cda33ae7f1262a3acf28",
+    "url": "/static/js/2.a0b7bce4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a0b7bce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9acd06c2d23a1de3db",
+    "url": "/static/js/main.6d685727.chunk.js"
+  },
+  {
+    "revision": "4b1bd734b8a2e32a83b1",
+    "url": "/static/js/runtime-main.b3a29566.js"
+  }
+]);
